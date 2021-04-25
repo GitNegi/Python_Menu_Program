7dbd6e547190f46f1d0367c4b8daf140151d7e44
Bhavesh <bhaveshkak26122000@gmail.com>
2021-04-25 07:26:14 -0400

    Added: Security Group, Volumes

--- aws/ec2.py
+++ aws/ec2.py
@@ -1,4 +1,4 @@
-import boto3
+import boto3, os
 
 def create_keypair(x):
     ec2 = boto3.resource('ec2')
@@ -9,17 +9,27 @@ def create_keypair(x):
     outfile.write(KeyPairOut)
     outfile.close()
 
-def create_sg(x):
-    pass
+    print("Key pair", x, "created successfully and public key saved in this directory.")
 
-def create_instance():
+def create_sg(x):
+    os.system("aws ec2 create-security-group --group-name {} --description my_{}".format(x,x))
+    os.system("aws ec2 authorize-security-group-ingress --group-name {} --protocol tcp --port 80 --cidr 0.0.0.0/0")
+
+    print("Security group", x, "created successfully.")
+
+def create_instance(keyname, sg):
     ec2_client = boto3.client("ec2", region_name="ap-south-1")
     instances = ec2_client.run_instances(
         ImageId="ami-0b0154d3d8011b0cd",
         MinCount=1,
         MaxCount=1,
         InstanceType="t2.micro",
-        KeyName="ec2-key-pair"
+        KeyName=keyname
     )
 
     print("Instance", instances["Instances"][0]["InstanceId"], "created successfully.")
+
+def ebs_volume(x):
+    os.system("aws ec2 create-volume --availability-zone ap-south-1 --size {}",format(int(x)))
+
+    print("Volume of size", x, "created successfully.")
